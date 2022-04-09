dd4f134cac62df00dbca9884e2f67a01554cca57
Crosbieybarra5 <89161534+Crosbieybarra5@users.noreply.github.com>
2022-04-08 19:25:18 -0500

    Add files via upload

---
+++ server/controllers/recipeController.js
@@ -0,0 +1,128 @@
+require('../models/database');
+const Category = require('../models/Category');
+const Recipe = require('../models/Recipe');
+
+// Homepage
+exports.homepage = async(req, res) => {
+  try {
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+    const latest = await Recipe.find({}).sort({_id: -1}).limit(limitNumber);
+    const thai = await Recipe.find({ 'category': 'Thai' }).limit(limitNumber);
+    const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
+    const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
+
+    const food = { latest, thai, american, chinese };
+
+    res.render('index', { title: 'RecipeBook - Home', categories, food } );
+  } catch (error) {
+    res.status(500).send({message: error.message || "Error Occured" });
+  }
+}
+
+// Categories
+exports.exploreCategories = async(req, res) => {
+  try {
+    const limitNumber = 20;
+    const categories = await Category.find({}).limit(limitNumber);
+    res.render('categories', { title: 'RecipeBook - Categories', categories } );
+  } catch (error) {
+    res.status(500).send({message: error.message || "Error Occured" });
+  }
+} 
+
+
+// Categories by Id
+exports.exploreCategoriesById = async(req, res) => { 
+  try {
+    let categoryId = req.params.id;
+    const limitNumber = 20;
+    const categoryById = await Recipe.find({ 'category': categoryId }).limit(limitNumber);
+    res.render('categories', { title: 'RecipeBook - Categories', categoryById } );
+  } catch (error) {
+    res.status(500).send({message: error.message || "Error Occured" });
+  }
+} 
+ 
+// Recipe
+exports.exploreRecipe = async(req, res) => {
+  try {
+    let recipeId = req.params.id;
+    const recipe = await Recipe.findById(recipeId);
+    res.render('recipe', { title: 'RecipeBook - Recipes', recipe } );
+  } catch (error) {
+    res.status(500).send({message: error.message || "Error Occured" });
+  }
+} 
+  
+
+exports.searchRecipe = async(req, res) => {
+    try {
+      let searchTerm = req.body.searchTerm;
+      let recipe = await Recipe.find( { $text: { $search: searchTerm, $diacriticSensitive: true } });
+      res.render('search', { title: 'RecipeBook - Search', recipe } );
+    } catch (error) {
+      res.status(500).send({message: error.message || "Error Occured" });
+    }
+    
+  }
+
+
+  exports.exploreLatest = async(req, res) => {
+    try {
+      const limitNumber = 20;
+      const recipe = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+      res.render('explore-latest', { title: 'Cooking Blog - Browse Recipes', recipe } );
+    } catch (error) {
+      res.status(500).send({message: error.message || "Error Occured" });
+    }
+  } 
+
+  exports.submitRecipe = async(req, res) => {
+    const infoErrorsObj = req.flash('infoErrors');
+    const infoSubmitObj = req.flash('infoSubmit');
+    res.render('submit-recipe', { title: 'RecipeBook - Submit Recipe', infoErrorsObj, infoSubmitObj  } );
+  }
+  
+  exports.submitRecipeOnPost = async(req, res) => {
+    try {
+  
+      let imageUploadFile;
+      let uploadPath;
+      let newImageName;
+  
+      if(!req.files || Object.keys(req.files).length === 0){
+        console.log('No Files where uploaded.');
+      } else {
+  
+        imageUploadFile = req.files.image;
+        newImageName = Date.now() + imageUploadFile.name;
+  
+        uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
+  
+        imageUploadFile.mv(uploadPath, function(err){
+          if(err) return res.satus(500).send(err);
+        })
+  
+      }
+  
+      const newRecipe = new Recipe({
+        name: req.body.name,
+        description: req.body.description,
+        email: req.body.email,
+        ingredients: req.body.ingredients,
+        category: req.body.category,
+        image: newImageName
+      });
+      
+      await newRecipe.save();
+  
+      req.flash('infoSubmit', 'Recipe has been added.')
+      res.redirect('/submit-recipe');
+    } catch (error) {
+      req.flash('infoErrors', error);
+      res.redirect('/submit-recipe');
+    }
+  }
+
+  
